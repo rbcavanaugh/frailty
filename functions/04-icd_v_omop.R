fca451e4a90162b9a7cc80158a51b5cf3598e4e0
Rob Cavanaugh <46727239+rbcavanaugh@users.noreply.github.com>
2024-10-03 10:42:11 -0400

    figs

--- functions/04-icd_v_omop.R
+++ functions/04-icd_v_omop.R
@@ -81,31 +81,3 @@ df_plot = left_join(omop_cats, icd_cats, by = c("category")) #%>% filter(categor
 
 write.csv(df_plot, "output/data_for_icc.csv")
 
-plotly::ggplotly(df_plot |>
-                     ggplot(aes(x = sum_score.x, y = sum_score.y, color = category)) +
-                     geom_point() +
-                     geom_abline() +
-                     labs(x = "omop", y = "icd"))
-
-df_plot |>
-    mutate(sum_score.x = sum_score.x/5292854,
-           sum_score.y = sum_score.y/5292854) %>%
-    ggplot(aes(x = sum_score.x, y = sum_score.y)) +
-    geom_point() +
-    geom_abline() +
-    labs(x = "OMOP Concept ID Prevalence", y = "ICD-10 Prevalence") +
-    theme_minimal(base_size = 16) +
-    scale_x_continuous(labels = scales::percent, limits = c(0, 0.5)) +
-    scale_y_continuous(labels = scales::percent, limits = c(0, 0.5))
-
-ggsave("output/supplement_p1.png", width = 8, height = 8)
-
-
-df_plot_noHTN = left_join(omop_cats, icd_cats, by = c("category")) %>% filter(category != "HTN")
-
-cor(df_plot$sum_score.x, df_plot$sum_score.y, use = "complete.obs")
-cor(df_plot_noHTN$sum_score.x, df_plot_noHTN$sum_score.y, use = "complete.obs")
-
-icc1 = irr::icc(df_plot %>% select(-category), model = "oneway", type = "agreement")
-icc2 = irr::icc(df_plot_noHTN %>% select(-category), model = "oneway", type = "agreement")
-
